915843725db1274d73bff935e11d177d349643db
Yuta Nakayama <y173345@edu.tut.ac.jp>
2022-01-20 21:08:40 +0900

    [add] task contents

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=128)
+    issue_date = models.DateTimeField('issue date')
+    complete_date = models.DateTimeField()
+    deadline = models.DateField()
+    is_finished = models.BooleanField(default=False)   
